72625d8530b5a18ef79cdad490b58839c18feaaa
aasiflm10 <94681493+aasiflm10@users.noreply.github.com>
2022-10-09 16:32:52 +0530

    Final Commit

--- app/src/main/java/com/example/gdsccomposecampfirstappday2/MainActivity.kt
+++ app/src/main/java/com/example/gdsccomposecampfirstappday2/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.gdsccomposecampfirstappday2
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -28,22 +25,42 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp() {
-    Surface(
-        color = MaterialTheme.colors.background
+fun MyApp(names : List<String> = listOf("Android", "Compose", "World")) {
+    Column(
+        modifier = Modifier.padding(4.dp)
     ) {
-        Greeting("Android")
+        for(name in names)
+        {
+            Greeting(name = name)
+        }
     }
 }
 @Composable
 fun Greeting(name: String) {
+    val expanded = remember{ mutableStateOf(false) }
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
 
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+
+    Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
+                Text(text = "Hello ")
+                Text(text = "$name!")
+            }
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                if(expanded.value)
+                    Text("Show Less")
+                else
+                    Text("Show More")
+
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 350)
 @Composable
 fun DefaultPreview() {
     GDSCComposeCampFirstAppDay2Theme {
